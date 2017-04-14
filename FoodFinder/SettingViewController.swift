9190784fd3acce2bca25c07c5f41ba50489894cb
Macbook <macbook@Macbooks-MacBook-Pro.local>
2017-04-14 15:13:23 -0500

    Setup the PickerView in Settings page

--- FoodFinder/SettingViewController.swift
+++ FoodFinder/SettingViewController.swift
@@ -8,8 +8,13 @@
 
 import UIKit
 
-class SettingViewController: UIViewController {
+class SettingViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
 
+    @IBOutlet weak var NumberOfResultsTextBox: UITextField!
+    @IBOutlet weak var NumberOfResultsDropdown: UIPickerView!
+    var resultsList = ["10","20","30","40","50"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,15 +26,35 @@ class SettingViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // This function assists the dropdown menu
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int{
+        return 1
     }
-    */
+    
+    // This function assists the dropdown menu
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
+        return resultsList.count
+    }
+    
+    // This function assists the dropdown menu
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        self.view.endEditing(true)
+        return resultsList[row]
+    }
+    
+    // This function assists the dropdown menu
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.NumberOfResultsTextBox.text = self.resultsList[row]
+        self.NumberOfResultsDropdown.isHidden = true
+    }
+    
+    // This function assists the dropdown menu
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField == self.NumberOfResultsTextBox {
+            self.NumberOfResultsDropdown.isHidden = false
+            //Disables seeing the keyboard
+            textField.endEditing(true)
+        }
+    }
 
 }
